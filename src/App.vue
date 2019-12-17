8791e626749f5c322fe7e704ddcaf6b634efc638
Лев Милов <milov@ir-tech.ru>
2019-12-17 20:52:01 +0400

    add

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,49 @@
 <template>
-    <div class="container text-center pt-5">
+    <div  class="container text-center pt-5">
+      <div id="smartquizLayout">
+
       
 
-<b-form-group
-      id="fieldset-1"
-      description="Let us know your name."
-      label="Enter your name"
-      label-for="input-1"
-      :invalid-feedback="invalidFeedback"
-      :valid-feedback="validFeedback"
-      :state="state"
-    >
-      <b-form-input id="input-1" v-model="name" :state="state" trim></b-form-input>
-    </b-form-group>
+        <b-form-group
+            id="fieldset-1"
+            description="Let us know your name."
+            label="Enter your name"
+            label-for="input-1"
+            :invalid-feedback="invalidFeedback"
+            :valid-feedback="validFeedback"
+            :state="state"
+          >
+            <b-form-input id="input-1" v-model="name" :state="state" trim></b-form-input>
+          </b-form-group>
 
+
+                    
+      <popper
+          trigger="clickToToggle"
+          :options="{
+            placement: 'top',
+            modifiers: { 
+              offset: { offset: '0,10px' } 
+            },
+            removeOnDestroy: false
+          }">
+          <div class="popper">
+            Popper Content
+          </div>
+
+          <button slot="reference">
+            Reference Element
+          </button>
+        </popper>            
 
               
-<popper
-    trigger="clickToToggle"
-    :options="{
-      placement: 'top',
-      modifiers: { 
-        offset: { offset: '0,10px' } 
-      },
-      removeOnDestroy: false
-    }">
-    <div class="popper">
-      Popper Content
-    </div>
-
-    <button slot="reference">
-      Reference Element
-    </button>
-  </popper>            
-
-        
-      <app-form-sending></app-form-sending>
-      <hr>
-      <app-Form-Modal v-if="answer"> </app-Form-Modal>
-      <app-Test></app-Test>
-      <app-Test2></app-Test2>
-
+            <app-form-sending></app-form-sending>
+            <hr>
+            <app-Form-Modal v-if="answer"> </app-Form-Modal>
+            <app-Test></app-Test>
+            <app-Test2></app-Test2>
+            
+      </div>
     </div>
 </template>
 
